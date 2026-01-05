71781082975a5102b993534aaf7c1108960aecbd
phanendra695-crypto <phanendra695@gmail.com>
2026-01-05 15:04:33 +0530

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'tournament_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
