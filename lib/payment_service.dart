2c97ea28bb104d1d92d74542462f7289ba7e59a8
phanendra695-crypto <phanendra695@gmail.com>
2026-01-05 15:05:28 +0530

    Create payment_service.dart

---
+++ lib/payment_service.dart
@@ -0,0 +1,43 @@
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:flutter/material.dart';
+
+class PaymentService {
+  late Razorpay _razorpay;
+
+  PaymentService() {
+    _razorpay = Razorpay();
+  }
+
+  void init({
+    required VoidCallback onSuccess,
+    required VoidCallback onFailure,
+  }) {
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, (_) => onSuccess());
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, (_) => onFailure());
+  }
+
+  void openCheckout({
+    required int amount,
+    required String description,
+    required String email,
+  }) {
+    var options = {
+      'key': 'RAZORPAY_KEY_ID', // 🔴 replace later
+      'amount': amount * 100, // in paise
+      'name': 'ArenaClash',
+      'description': description,
+      'prefill': {
+        'email': email,
+      },
+      'theme': {
+        'color': '#7C4DFF',
+      }
+    };
+
+    _razorpay.open(options);
+  }
+
+  void dispose() {
+    _razorpay.clear();
+  }
+}
