c1a26e76d449dec97f0b14e28882c6a6806fbe36
phanendra695-crypto <phanendra695@gmail.com>
2026-01-05 15:00:38 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const ArenaClashApp());
+}
+
+class ArenaClashApp extends StatelessWidget {
+  const ArenaClashApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ArenaClash',
+      theme: ThemeData.dark(),
+      home: const LoginScreen(),
+    );
+  }
+}
