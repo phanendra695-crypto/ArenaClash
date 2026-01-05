e7a3d8922bb91914c6a7240d9708c19349e83bd2
phanendra695-crypto <phanendra695@gmail.com>
2026-01-05 15:02:46 +0530

    Create home_screen.dart

---
+++ lib/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ArenaClash'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to ArenaClash 🎮',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
